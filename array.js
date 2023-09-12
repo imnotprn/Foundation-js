07fec1bae8d0c76f8830e965c595f57f714af751
Macbook <macbook@Macbooks-MacBook-Air.local>
2023-09-12 12:17:32 -0700

    add imnotprn file

---
+++ array.js
@@ -0,0 +1,17 @@
+// * Exercise
+// ? Create an array that contain 4 foods that you like. Then complete these following tasks.
+const myFavoriteFood = ["tomyam", "kawpad", "koi", "larb"];
+console.log(myFavoriteFood);
+
+// ? 1. Locate your most favorite food in the array. Print its array index to the console.
+myFavoriteFood.push("chanom", "iscream", "toast");
+myFavoriteFood.indexOf();
+console.log(myFavoriteFood.indexOf("tomyam"));
+console.log(myFavoriteFood);
+
+// ? 2. Add your 3 favorite desserts or fruits into that array. Print the result to the console.
+
+// ? CHALLENGES: Look at the Array methods slide. Pick one method to check if your most favorite food is existed in the array or not? (hint: return as boolean)
+
+myFavoriteFood.includes();
+console.log(myFavoriteFood.includes("tomyam"));
